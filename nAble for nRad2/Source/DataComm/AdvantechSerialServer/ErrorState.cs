b7601491af96eec922dda4781879c25a3a8c10f1
Jason Mahan <J.Mahan@nTact.com>
2023-12-19 18:22:50 -0600

    Fixed usings on this file

--- nAble for nRad2/Source/DataComm/AdvantechSerialServer/ErrorState.cs
+++ nAble for nRad2/Source/DataComm/AdvantechSerialServer/ErrorState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace nAble.DataComm.AdvantechSerialServer
+﻿namespace nAble.DataComm.AdvantechSerialServer
 {
     public enum ErrorStates
     {
